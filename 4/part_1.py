b3e5f185f6cfe749a7902499d4fdb5c87ccecc96
Henri Lemoine <henri123lemoine@gmail.com>
2023-12-05 20:52:42 -0500

    Added AoC solution for Day 4

---
+++ 4/part_1.py
@@ -0,0 +1,20 @@
+import sys
+data = sys.stdin.readlines()
+
+sol = 0
+for i, line in enumerate(data):
+    line = line.split(": ")[1]
+    line = line.split(" | ")
+    line = [l.split(" ") for l in line]
+    line = [[l.strip() for l in l] for l in line]
+    line = [[int(l) for l in l if l] for l in line]
+
+    winning_numbers, numbers_you_have = line
+
+    round_points = -1
+    for winning_number in winning_numbers:
+        if winning_number in numbers_you_have:
+            round_points += 1
+    sol += round(2**round_points)
+
+print(f"sol: {sol}")
---
+++ 4/part_2.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys
+data = sys.stdin.readlines()
+
+num_instances = np.ones(len(data))
+
+for i, line in enumerate(data):
+    line = line.split(": ")[1]
+    line = line.split(" | ")
+    line = [l.split(" ") for l in line]
+    line = [[l.strip() for l in l] for l in line]
+    line = [[int(l) for l in l if l] for l in line]
+
+    winning_numbers, numbers_you_have = line
+    round_points = 0
+    for winning_number in winning_numbers:
+        if winning_number in numbers_you_have:
+            round_points += 1
+    num_instances[i+1:i+round_points+1] += num_instances[i]
+
+sol = int(num_instances.sum())
+print(f"sol: {sol}")
