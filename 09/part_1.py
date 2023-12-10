8420818f153787dde7c5f341d7d1c8da3a40728c
Henri Lemoine <henri123lemoine@gmail.com>
2023-12-10 01:10:28 -0500

    Added AoC solution for Day 9

--- 09/part_1.py
+++ 09/part_1.py
@@ -1,19 +1,18 @@
 import sys
 
-PATTERN = sys.stdin.readline().strip()
-sys.stdin.readline()
-
-_map = {}
+sol = 0
 data = sys.stdin.readlines()
 for line in data:
-    l, r = line.split(" = (")[-1].replace(" ", "").replace(")", "").strip().split(",")
-    _map[line.split(" = (")[0]] = (l, r)
-
-sol = 0
-curr = "AAA"
-while curr != "ZZZ":
-    next_node = _map[curr][PATTERN[sol % len(PATTERN)] == "R"]
-    sol += 1
-    curr = next_node
-
+    line = line.strip()
+    numbers = [int(x) for x in line.split(" ")]
+    line_sol = numbers[-1]
+    while True:
+        new_numbers = []
+        for i in range(len(numbers) - 1):
+            new_numbers.append(numbers[i + 1] - numbers[i])
+        numbers = new_numbers
+        line_sol += numbers[-1]
+        if all(x == 0 for x in numbers):
+            break
+    sol += line_sol
 print(sol)
--- 09/part_2.py
+++ 09/part_2.py
@@ -1,60 +1,25 @@
 import sys
-from itertools import permutations
-from math import lcm
 
-PATTERN = sys.stdin.readline().strip()
-sys.stdin.readline()
-
-_map = {}
+sol = 0
 data = sys.stdin.readlines()
-A_count = 0
-starting_nodes = []
-ending_nodes = []
 for line in data:
-    key = line.split(" = (")[0]
-    if key.endswith("A"):
-        A_count += 1
-        starting_nodes.append(key)
-    if key.endswith("Z"):
-        ending_nodes.append(key)
-    l, r = line.split(" = (")[-1].replace(" ", "").replace(")", "").strip().split(",")
-    _map[key] = (l, r)
-
-print(starting_nodes, ending_nodes)
-
-def find_distance(starting_node, ending_node, _map):
-    sol = 0
-    curr = starting_node
-    while curr != ending_node:
-        next_node = _map[curr][PATTERN[sol % len(PATTERN)] == "R"]
-        sol += 1
-        curr = next_node
-        if sol > 1e6:
-            return 1_000_000
-    return sol
-
-
-# For each permutation of starting-point-ending-point link, find out how many steps (if ever) it takes to reach it from the starting point
-# Then, for each permutation, find the LCM of the steps for each link, and take the smallest such
-
-lcms = []
-for i, permutation in enumerate(permutations(ending_nodes)):
-    steps = []
-    for j, (starting_node, ending_node) in enumerate(zip(starting_nodes, permutation)):
-        dist = find_distance(starting_node, ending_node, _map)
-        steps.append(dist)
+    line = line.strip()
+    numbers = [int(x) for x in line.split(" ")]
+    differences = []
+    differences.append(numbers)
+    while True:
+        new_numbers = []
+        for i in range(len(numbers) - 1):
+            new_numbers.append(numbers[i + 1] - numbers[i])
+        numbers = new_numbers
+        differences.append(numbers)
+        if all(x == 0 for x in numbers):
+            break
     
-    print(permutation, steps)
+    differences[-1].insert(0, 0)
+    for i in range(len(differences) - 1, 0, -1):
+        differences[i - 1].insert(0, differences[i - 1][0] - differences[i][0])
     
-    # Find the LCM of the steps for each link
-    lowest_common_multiple = lcm(*steps)
-    lcms.append(lowest_common_multiple)
-    print(lowest_common_multiple)
-
-print(lcms)
-print(min(lcms))
-
-
-# This was horrible; the way I got the answer was by running the program, observing the single time the steps contained no 1e6 values, and calculated the lcm for that one by running the code below
-from math import lcm
-print(lcm(11567, 21251, 12643, 16409, 19099, 14257))
+    sol += differences[0][0]
+    
+print(sol)
